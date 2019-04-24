fb7b42aa015652d1ab5a07c37a6dc59d92497e7a
k-lpmg <kanglpmg@gmail.com>
2019-04-24 18:11:53 +0900

    Delete main storyboard in Example

--- ScratchImageExample/Sources/AppDelegate.swift
+++ ScratchImageExample/Sources/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let rootViewController = ViewController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
     
--- ScratchImageExample/Sources/ViewController.swift
+++ ScratchImageExample/Sources/ViewController.swift
@@ -11,24 +11,83 @@ import ScratchImage
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var scratchImageView: ScratchImageView! {
-        didSet {
-            scratchImageView.backgroundImageColor = UIColor.brown
-            scratchImageView.delegate = self
-        }
-    }
-    @IBOutlet weak var percentLabel: UILabel! {
-        didSet {
-            percentLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        }
+    // MARK: - UI Components
+    
+    private let resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("reset", for: .normal)
+        return button
+    }()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "image.jpg"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    private let scratchImageView: ScratchImageView = {
+        let scratchImageView = ScratchImageView(imageColor: UIColor.brown)
+        scratchImageView.translatesAutoresizingMaskIntoConstraints = false
+        return scratchImageView
+    }()
+    private let percentLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    // MARK: - Overridden: UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setProperties()
+        setSelector()
+        view.addSubview(resetButton)
+        view.addSubview(imageView)
+        view.addSubview(scratchImageView)
+        view.addSubview(percentLabel)
+        layout()
     }
     
-    @IBAction func resetButtonClicked(_ sender: Any) {
+    // MARK: - Private methods
+    
+    private func layout() {
+        resetButton.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 32).isActive = true
+        resetButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        
+        imageView.topAnchor.constraint(equalTo: resetButton.bottomAnchor, constant: 32).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        scratchImageView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        scratchImageView.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        scratchImageView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        scratchImageView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        
+        percentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        percentLabel.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -32).isActive = true
+    }
+    
+    private func setProperties() {
+        view.backgroundColor = .white
+        scratchImageView.delegate = self
+    }
+    
+    private func setSelector() {
+        resetButton.addTarget(self, action: #selector(resetButtonClicked), for: .touchUpInside)
+    }
+    
+    // MARK: - Private selector
+    
+    @objc private func resetButtonClicked() {
         scratchImageView.reset()
         percentLabel.text = nil
     }
     
 }
+
 
 // MARK: - ScratchImageViewDelegate
 
