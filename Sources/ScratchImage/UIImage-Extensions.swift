a1d9080811804ce8345794bf3d36c1259f865c5c
k-lpmg <kanglpmg@gmail.com>
2019-04-25 11:56:22 +0900

    Add UIView extensions

--- Sources/ScratchImage/UIImage-Extensions.swift
+++ Sources/ScratchImage/UIImage-Extensions.swift
@@ -1,9 +1,9 @@
 //
-//  UIImageExtension.swift
+//  UIImage-Extensions.swift
 //  ScratchImage
 //
-//  Created by DongHeeKang on 2018. 5. 17..
-//  Copyright © 2018년 k-lpmg. All rights reserved.
+//  Created by Cashwalk on 2018. 5. 17..
+//  Copyright © 2018년 Cashwalk. All rights reserved.
 //
 
 import UIKit
@@ -13,9 +13,9 @@ extension UIImage {
     static func fromColor(color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()
-        context!.setFillColor(color.cgColor)
-        context!.fill(rect)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image!
---
+++ Sources/ScratchImage/UIView-Extensions.swift
@@ -0,0 +1,49 @@
+//
+//  UIView-Extensions.swift
+//  ScratchImage
+//
+//  Created by Cashwalk on 25/04/2019.
+//  Copyright © 2019 Cashwalk. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+extension UIView {
+    func getSnapshot() -> CGImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
+        guard let ctx = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        
+        layer.render(in: ctx)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image?.cgImage
+    }
+    
+    func getTransparentPixelsPercent() -> Double {
+        guard let image = getSnapshot(), let imageData = image.dataProvider?.data else { return 0.0 }
+        
+        let width = image.width
+        let height = image.height
+        let imageDataPointer: UnsafePointer<UInt8> = CFDataGetBytePtr(imageData)
+        var transparentPixelCount = 0
+        
+        for x in 0...width {
+            for y in 0...height {
+                let pixelDataPosition = ((width * y) + x) * 4
+                // The alpha value is the last 8 bits of the data
+                let alphaValue = imageDataPointer[pixelDataPosition + 3]
+                if alphaValue == 0 {
+                    transparentPixelCount += 1
+                }
+            }
+        }
+        
+        var transparentPercent = Double(transparentPixelCount) / Double((width * height))
+        transparentPercent = max(transparentPercent, 0)
+        transparentPercent = min(transparentPercent, 1)
+        return transparentPercent
+    }
+}
